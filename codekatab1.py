2780d85a4d5c3cd3057a5aa7dcbc7bb0d0ed0ac0
reach2arunprakash <reach2arunprakash@gmail.com>
2019-03-06 14:33:48 +0530

    Create codekatab1.py

---
+++ codekatab1.py
@@ -0,0 +1,9 @@
+#Getting the input from the user
+number=int(raw_input(""))
+#Checking whether the number is Positive or Negative Or Zero
+if(number>0):
+	print("Positive")
+elif(number<0):
+	print("Negative")
+else:
+	print("Zero")
